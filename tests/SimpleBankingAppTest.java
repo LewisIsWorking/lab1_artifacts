66a6ec0d3f39edd82c62a56489e3cfd976196ad7
Lewis <lcreelman01@qub.ac.uk>
2022-02-26 21:09:32 +0000

    3.6-Automated testing of the main app
    Signed-off-by: Lewis <lcreelman01@qub.ac.uk>

--- tests/SimpleBankingAppTest.java
+++ tests/SimpleBankingAppTest.java
@@ -50,16 +50,30 @@ public class SimpleBankingAppTest {
 	}
 
 	// this test method (test case) verifies if the Withdraw feature works properly
-	/* TODO
+	// TODO
 	public static void testWithdrawals() {
-		...
+		// Setup phase
+		double balanceBefore = mainApp.getBalance("5495-1234");
+		// Negative Value to reduce balance.
+		double withdrawAmount = -21.50;
+
+		// Exercise phase
+		mainApp.addTransaction("5495-1234", withdrawAmount);
+		double balanceAfter = mainApp.getBalance("5495-1234");
+		// The balance from before this test minus the amount withdrawn should be equal to the new account balance.
+		assert balanceBefore - withdrawAmount == balanceAfter;
+		System.out.println("testDeposits: TC1 passed.");
+
+		// tear-down: put the system state back in where it was
+		// Add back the value withdrawn.
+		mainApp.addTransaction("5495-1234", withdrawAmount);
 	}
-	*/
+
 	
 	public static void main(String[] args) {
 		testDataLoads();
 		testDeposits();
-		// testWithdrawals(); -- uncomment this call, when you have developed the test method (test case)
+		testWithdrawals(); //uncomment this call, when you have developed the test method (test case)
 	}
 
 }
